875543fb3c18a644be2deb3574880471c138a9d8
Vedant Mistry <mistryvedant17@gmail.com>
2022-01-29 10:59:10 +0530

    javascript fundamentals 2 strings

---
+++ javascriptFundamentals2/jsFundamentals2.js
@@ -0,0 +1,10 @@
+const string = "hello world!";
+console.log(string);
+
+const string1 = "this is a string";
+//concatenating strings 
+const greeting = `hello world!, ${string1}`; //by using the `` quotating marks, concatentating of strings is possible.
+console.log(greeting);
+
+
+
