85fe2159bfca8ba07e8174ad45b65d85109d20e9
Vedant Mistry <mistryvedant17@gmail.com>
2022-02-03 01:07:13 +0530

    solving the fizzbuzz question of theodinproject

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,11 +1,14 @@
-let limit = 15;
+let limit = 20;
 
 for (let i=1; i<=limit; i++) {
-    if (i%3 == 0) {
-        console.log("fizz");
+    if (i%3 === 0 && i%5 === 0) {
+        console.log("fizzbuzz");
     }
     else if (i%5 == 0) {
         console.log("buzz");
+    }
+    else if (i%3 == 0) {
+        console.log("fizz");
     }
     else {
         console.log(i);
