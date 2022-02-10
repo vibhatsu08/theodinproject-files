f03a788d35ea6b6072b3c26ae46576331915249d
Vedant Mistry <mistryvedant17@gmail.com>
2022-02-10 12:46:28 +0530

    remaking the DOM website.

--- DOMmanipulation/script.js
+++ DOMmanipulation/script.js
@@ -1,15 +1,32 @@
-const container = document.querySelector("#container");
+const container = document.querySelector('#container'); //pointing towards the parent div
+const content = document.createElement('div'); //creating an element 'content' unded the parent div.
+content.classList.add('content'); //adds the content div to the classlist.
+content.textContent = 'This is some glorious text content.'; //adds text to the content div.
+container.appendChild(content); //appending the content div to the container div.
 
-const red_para = document.createElement("p");
-red_para.classList.add("red_para");
-red_para.style.color = "red";
-red_para.style.cssText("Hey I am red!");
+let para = document.createElement('p');
+para.textContent = 'Hey I\'m red!';
+para.style.color = 'red';
+let element = document.getElementById('container');
+element.appendChild(para);
 
-const blue_header = document.createElement("h3");
-blue_header.classList("blue_header");
-blue_header.style.color = "blue";
-blue_header.style.cssText("I'm a blue header");
+let header_3 = document.createElement('h3');
+header_3.textContent = 'Hey I\'m a blue h3!';
+header_3.style.color = 'blue';
+element.appendChild(header_3);
 
-const new_div = document.createElement("div");
-new_div.classList("new_div");
-new_div.setAttribute("style", "border: black; background: pink");
+let new_div = document.createElement('div');
+new_div.setAttribute('style', 'border: black; background-color: pink');
+element.appendChild(new_div);
+let element_div = document.getElementById('new_div');
+
+let header_1 = document.createElement('h1');
+header_1.textContent = 'I\'m in a div!';
+new_div.appendChild(header_1);
+
+let new_para = document.createElement('p');
+new_para.textContent = 'ME TOO!';
+new_div.appendChild(new_para);
+
+document.body.insertBefore(new_div, null);
+
