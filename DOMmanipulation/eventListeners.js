316ce9f84b49b88bc8fdc44ffb5326023dc28bb3
Vedant Mistry <mistryvedant17@gmail.com>
2022-02-10 19:57:00 +0530

    Event Listeners in DOM

--- DOMmanipulation/eventListeners.js
+++ DOMmanipulation/eventListeners.js
@@ -1,22 +1,29 @@
-const button = document.querySelector('#btn');
-button.addEventListener (
-    'click', () => {
-        alert("hello world!");
-    }
-);
+// const button = document.querySelector('#btn');
+// button.addEventListener (
+//     'click', () => {
+//         alert("hello world!");
+//     }
+// );
 
-function alertFunction () {
-    alert("hello world!");
-}
+// function alertFunction () {
+//     alert("hello world!");
+// }
 
-const buttons = document.querySelectorAll('button');
-buttons.forEach ((button) =>
-    {
-        button.addEventListener(
-            'click', () => {
-                alert("hello world!");
-            }
-        );
+// const buttons = document.querySelectorAll('button');
+// buttons.forEach ((button) =>
+//     {
+//         button.addEventListener(
+//             'click', () => {
+//                 alert("hello world!");
+//             }
+//         );
+//     }
+// );
+
+const new_button = document.querySelector('#new-button');
+new_button.addEventListener (
+    'click', function (e) {
+        console.log(e);
     }
 );
 
