2e20cc6918f0a084277c30c59fc23bbf589dcb88
Vedant Mistry <mistryvedant17@gmail.com>
2022-04-17 16:15:19 +0530

    conditionals in javascript

---
+++ refreshingJavascript/part3/script3.js
@@ -0,0 +1,10 @@
+// conditions in javascript
+
+let icecream = "vanilla";
+
+if (icecream == "chocolate") {
+    console.log("icecream is chocolate");
+}
+else {
+    console.log("icecream is not chocolate");
+}
