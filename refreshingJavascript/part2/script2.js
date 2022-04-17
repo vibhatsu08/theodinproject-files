313d3afa8c84c7553ed4d7b8fa2e1987d5999078
Vedant Mistry <mistryvedant17@gmail.com>
2022-04-17 16:12:52 +0530

    variables in javascript

---
+++ refreshingJavascript/part2/script2.js
@@ -0,0 +1,18 @@
+// creating variables in javascript
+//creating a string in javscript
+let myVariable = 'Bob';
+console.log(myVariable);
+
+// creating an int in javascript
+let myNumber = 20;
+console.log(myNumber);
+
+// creating a boolean in javascript
+let myBoolean = true;
+console.log(myBoolean);
+
+// creating an array in javascript
+let myArray = ["tony", "stark", 57, true, "ironman"];
+console.log(myArray);
+console.log(myArray[2]);
+console.log(myArray[1])
