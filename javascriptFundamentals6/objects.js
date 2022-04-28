a4bc02f6365f67b276bb5e3d27748823850688fb
Vedant Mistry <mistryvedant17@gmail.com>
2022-04-28 17:13:02 +0530

    objects in javascript

---
+++ javascriptFundamentals6/objects.js
@@ -0,0 +1,11 @@
+// javascript program to experiment with objects in javascript.
+const newObject = {
+    property: "value",
+    otherProperty: "",
+    "otherProperty": function () {
+        // do stuff, random stuff
+    }
+}
+
+
+
